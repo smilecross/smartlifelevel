0de211dda4bf96d15ccfd22051f2f7b381b13c68
smilecross <crossheart44@gmail.com>
2023-07-14 11:13:32 +0900

    update md file

--- home1.php
+++ home1.php
@@ -26,6 +26,20 @@ try {
     $query = $pdo->query('SELECT * FROM questions_table');
     $result = $query->fetchAll();
 
+    $question_options = array();
+
+    foreach ($result as $row) {
+      if ($row['category_id'] == 1) {
+
+        // 質問に対応する選択肢を取得
+        $stmt = $pdo->prepare("SELECT * FROM options_table WHERE question_id = ?");
+        $stmt->execute([$row['question_id']]);
+        $options = $stmt->fetchAll();
+
+        $question_options[$row['question_text']] = $options;
+      }
+    }
+
 } catch (PDOException $e) {
     echo json_encode(["db error" => "{$e->getMessage()}"]);
     exit();
@@ -47,26 +61,45 @@ try {
 
 </head>
 <body class="md:flex mt-16 h-full font-mono">
-     <section class="bg-emerald-100 text-gray-800 flex flex-col w-3/4 mx-auto px-4 py-2 rounded-lg <?php if(!isset($_POST['username'])) { echo 'hidden'; } ?> ">
-        <h2 class="text-lg text-center"><?php echo $result[0]['question_text']; ?></h2>
-        <ul class="text-left space-y-4">
-            <?php
-    foreach ($result as $row) {
-      if ($row['category_id'] == 1) {
-        echo "<li>" . $row['question_text'] . "</li>";
-      }
-    }
-    ?>
-        </ul>
+
+ <section class="bg-emerald-100 text-gray-800 flex flex-col w-4/5 mx-auto px-4 py-2 rounded-lg <?php if(!isset($_SESSION['username'])) { echo 'hidden'; } ?> ">
+    <form action="score_process.php" method="post">
+    <h2 class="text-lg text-center">Questions and Options</h2>
+    <ul class="text-left text-lg space-y-4">
+       <?php
+        $questionCount = 0;
+        foreach ($question_options as $question => $options) {
+            echo "<li class='text-2xl font-bold'>" . $question . "</li>";
+            echo "<ul>";
+            foreach ($options as $option) {
+              echo "<li class='m-4'><input type='radio' name='scores[".$questionCount."]' value='" . $option['score'] . "' class='mr-2'>" . $option['option_text'] . "</li>";
+            }
+            echo "</ul>";
+            $questionCount++;
+        }
+        ?>
+    </ul>
+    </form>
     </section>
+
         <br>
         <div class="flex">
-            <div class="text-center bg-emerald-100 hover:bg-blue-500 w-1/6 mx-auto px-4 py-2 rounded-full">
-                <input type="submit" value="戻る">
-            </div>
-            <div class="text-center bg-emerald-100 hover:bg-blue-500 w-1/6 mx-auto px-4 py-2 rounded-full">
-                <input type="submit" value="次へ">
-            </div>
+            <button onclick="goBack()" class="text-center bg-emerald-100 hover:bg-blue-500 w-1/6 mx-auto px-4 py-2 rounded-full">
+            戻る
+            </button>
+            <button onclick="submitForm()" class="text-center bg-emerald-100 hover:bg-blue-500 w-1/6 mx-auto px-4 py-2 rounded-full">
+            次へ
+            </button>
         </div>
+
+        <script>
+            function goBack() {
+                window.history.back();
+            }
+
+            function submitForm() {
+                document.querySelector('form').submit();
+            }
+        </script>
 </body>
 </html>
